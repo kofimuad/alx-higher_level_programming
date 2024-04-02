2b10ec31a03585b0204efd354533dfa2ca02d4e0
kofimuad <iamkofiantwi@outlook.com>
2024-04-02 23:39:44 +0000

    first assign

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """A combination of liscomp, map and lambda"""
+    return ([list(map(lambda x: x * x ,row))for row in matrix])
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,4 @@
+# 0x04. Python - More Data Structures: Set, Dictionary
+
+## Task 0 - Squared simple
+A function that computes the square value of all integers of a matrix.
