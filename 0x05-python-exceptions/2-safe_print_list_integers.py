3edacc4bb7b66d4bd258c05f076dea9bd14bb664
kofimuad <iamkofiantwi@outlook.com>
2024-04-30 00:43:37 +0000

    changed last print and rearranged arguments in except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
-
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             ret += 1
-        except (TypeError, ValueError):
+        except (ValueError, TypeError):
             continue
-     print("")
+    print("")
     return (ret)
