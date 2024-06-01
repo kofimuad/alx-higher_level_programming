081eaf984a673ef48bd7c23ae6025ca219292003
kofimuad <iamkofiantwi@outlook.com>
2024-06-01 23:52:41 +0000

    task 2-divide matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A module that divides a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """divides each element of matrix by div"""
+
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+
+    new = []
+    rowlen = len(matrix[0])
+
+    for row in matrix:
+        sub = []
+        if len(row) != rowlen:
+            raise TypeError('Each row of the matrix must have the same size')
+        for i in range(len(row)):
+            if not isinstance(row[i], (int, float)):
+                raise TypeError('matrix must be a matrix (list of lists) of '
+                                'integers/floats')
+            sub.append(round((row[i] / div), 2))
+
+        new.append(sub)
+    return (new)
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -2,3 +2,6 @@
 
 ## Task 0 - Integers addition
 A function that adds two integers
+
+## Task 1 - Divide a matrix
+A function that divides all elements of a matrix
