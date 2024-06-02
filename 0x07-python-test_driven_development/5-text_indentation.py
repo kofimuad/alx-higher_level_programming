86b7740f02a0a12760fc75be5eda79d3f9d8c4ba
kofimuad <iamkofiantwi@outlook.com>
2024-06-02 17:44:50 +0000

    fixed pycodestyle

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,7 +8,7 @@ def text_indentation(text):
     """remove whitespace from start and end of line and add newline"""
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    for c in ['.',':','?']:
+    for c in ['.', ':', '?']:
         text = text.replace(c, c + '\n\n')
     new = [line.strip() for line in text.split('\n')]
     new = '\n'.join(new)
