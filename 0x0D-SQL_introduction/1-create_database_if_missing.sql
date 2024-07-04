9f767b1e43f4eb4191ae499acbbc8e2da8e0cc24
kofimuad <iamkofiagyei@gmail.com>
2024-07-04 15:24:42 +0000

    delete a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create a databse if it doesn't already exist.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Deletes databases if they exist.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
