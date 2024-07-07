096c776faf3f4eb4443a1363b66cb717db81fbfb
kofimuad <iamkofiagyei@gmail.com>
2024-07-07 03:06:47 +0000

    a sql keyword was in smaller letters

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@ SELECT t.title AS title
 FROM tv_shows AS t
 INNER JOIN tv_show_genres AS s
 ON t.id = s.show_id
-INNER JOIN tv_genres as g
+INNER JOIN tv_genres AS g
 ON g.id = s.genre_id
 WHERE g.name = 'Comedy'
 ORDER BY title;
